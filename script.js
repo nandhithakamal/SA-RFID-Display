77e352cccdb80351dc8c659d5d4666fa00edc779
nandhithakamal <nandhithakml@gmail.com>
2017-08-21 17:48:02 +0530

    RFID tags displayed live

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
 $(document).ready(function(){
-    var listItems = "Nothin'yet!";
-
     $("#count").html("5");
-    $("#rightCol").html(listItems);
-
-    loadTags();
-
-    function loadTags(){
-        $.ajax({
-            type: 'GET',
-            contentType: 'application/json',
-            url: 'http://localhost:9090/index/data',
-            success: function(tags){
-                /*if($("#rightCol").html == "--" || $("#rightCol").html === "Nothin' yet!"){
-                    $("#rightCol").html(tags + "\n");
-                }else{
-                    $('#rightCol').append(tags + "\n");
-                }*/
-                $("#rightCol").append("\n" + tags);
-            },
-
-            complete: setTimeout(loadTags, 1000)
-        });
-    }
-
-})
+});
--- server.js
+++ server.js
@@ -1,57 +1,23 @@
 var hapi = require('hapi');
 var serialport = require('serialport');
-var events = require('events');
-
 
 var server = new hapi.Server();
 var portName = 'COM5';
-var tags = '';
+var tag = '';
 var parsedData = undefined;
 
 var sp = new serialport(portName, {
     baudRate: 230400,
 });
 
-var eventEmitter = new events.EventEmitter();
-
-eventEmitter.on('tag', function(tag){
-    console.log("I saw " + tag);
-});
-
-sp.on('data', function(data) {
-    parsedData = data.toString();
-    //console.log("Parsed Data" + parsedData + typeof parsedData + parsedData.length);
-    if (parsedData.length > 3){
-        tags = parsedData;
-        eventEmitter.emit('tag', tags);
-        //console.log(tags);
-    }
-    else{
-        tags = undefined;
-        //console.log(tags);
-    }
-});
 
 server.connection({ port: 9090, host: 'localhost'});
 
-server.start((err) => {
-    if(err){
-        console.log(err);
-    }
-    else{
-        console.log(`Server on ${server.info.uri}`);
-    }
+var io = require('socket.io')(server.listener);
+
+io.on('connection', function(socket){
+    socket.emit('tagRead', "Nothing yet!" );
 });
-
-
-
-/*server.route({
-    method: 'GET',
-    path: '/{name}',
-    handler: function (request, reply) {
-        reply('Hello, ' + encodeURIComponent(request.params.name) + '! <br> I am seamless-access, the  all powerful and all encompassing!');
-    }
-});*/
 
 server.register(require('inert'), (err) => {
 
@@ -59,11 +25,20 @@ server.register(require('inert'), (err) => {
         throw err;
     }
 
+    server.start((err) => {
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log(`Server on ${server.info.uri}`);
+        }
+    });
+
     server.route({
         method: 'GET',
         path: '/index',
         handler: function (request, reply) {
-            reply.file('./index.html');
+            reply.file('index.html');
         }
     });
     server.route({
@@ -80,6 +55,13 @@ server.register(require('inert'), (err) => {
             reply.file('./script.js');
         }
     });
+    /*server.route({
+        method: 'GET',
+        path: '/socket.io/socket.io.js',
+        handler: function(request, reply){
+            reply.file('./node_modules/socket.io/lib/socket.js');
+        }
+    })*/
     server.route({
         method: 'GET',
         path: '/{name}',
@@ -94,11 +76,18 @@ server.register(require('inert'), (err) => {
             reply.redirect('/index');
         }
     });
-    server.route({
-        method: 'GET',
-        path: '/index/data',
-        handler: function(request, reply){
-            reply(tags);
-        }
-    });
+});
+
+sp.on('data', function(data) {
+    parsedData = data.toString();
+    //console.log("Parsed Data" + parsedData + typeof parsedData + parsedData.length);
+    if (parsedData.length > 3){
+        tag = parsedData;
+        io.emit('tagRead', tag);
+        //console.log(tags);
+    }
+    else{
+        tag = undefined;
+        //console.log(tags);
+    }
 });
